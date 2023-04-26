71f87f94cb40785764124b8dcf963e8c00565011
wlady3190 <wlady_3190@hotmail.com>
2023-04-26 07:57:09 -0500

    Adding footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BarraComponent } from './barra/barra.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BarraComponent
+    BarraComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
